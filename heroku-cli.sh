a71276b2a4ade181f2d16e26a3e008e435866f12
Mindaugas Urbontaitis <mindaugas@urbontaitis.lt>
2021-12-24 17:51:52 +0200

    cleanup

--- heroku-cli.sh
+++ heroku-cli.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
-heroku apps:create
-heroku addons:create heroku-postgresql:hobby-dev
-heroku config:set RUN_MIGRATIONS=1
-heroku config:set CREATE_ADMIN=1
-heroku config:set ADMIN_USERNAME=admin
-heroku config:set ADMIN_PASSWORD=test123
 git push heroku main
